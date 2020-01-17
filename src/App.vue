e571e903c4fc2eb191aae77789193fa90f7f25ed
Aaron Honson <aaron.shannon-honson@adelaide.edu.au>
2020-01-17 13:47:44 +1030

    More fixes

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,12 @@ export default {
   },
   created(){
     this.darkMode = localStorage.darkMode;
+    console.log(this.darkMode);
     //set the mode on the body on created
     if(this.darkMode == 'true'){
-        document.body.classList.remove('dark');
-        document.body.classList.add('light');
-      } else {
-        document.body.classList.remove('light');
         document.body.classList.add('dark');
+      } else {
+        document.body.classList.add('light');
       }
   },
   methods: {
